ae7a29195ded1903db57f6721ab66f276321972a
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-07-17 21:57:21 +0100

    reinport global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import HomeImg from "../../public/Home.jpg";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
