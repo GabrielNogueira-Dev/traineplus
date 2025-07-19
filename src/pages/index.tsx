5fa2885a3bf9fa9d1530ad25dd7d5f8b85fd473a
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-07-19 17:13:19 +0100

    metatas add for work in Linkedin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ export default function Home() {
     <div>
       <Head>
         <title>Home</title>
+        {/* Metatags Open Graph para LinkedIn e redes sociais */}
+        <meta property="og:title" content="Push YourSelf Harder - TrainePlus" />
+        <meta property="og:description" content="Desafie-se a ir além com TrainePlus, seu app para treino e motivação." />
+        
+        {/* URL da imagem - importante que seja pública e acessível */}
+        <meta property="og:image" content="https://traineplus.vercel.app/Home.jpg" />
+        <meta property="og:url" content="https://traineplus.vercel.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <main className={styles.mainimg}>
