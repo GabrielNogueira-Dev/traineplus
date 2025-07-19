85d2b525f0b871bf0427ec3cdfc3da028c63ca4b
Gabriel Nogueira <gabrielfariass-99@hotmail.com>
2025-07-19 18:47:17 +0100

    commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,23 +1,19 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import GitHubProvider  from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-    
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-      }),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     GitHubProvider({
-         clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    })
-
-    ],
-    secret: process.env.JWT_SECRET as string
-    
-   
-}
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET as string, 
+};
 
 export default NextAuth(authOptions);
